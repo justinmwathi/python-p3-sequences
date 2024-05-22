0eed0ca2df6e20a2a9cefba0a0a1a0ad0206b1f0
justin <justinmwathi32@gmail.com>
2024-05-22 14:00:31 +0300

    Lab completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
-
+fib_series = [0, 1]
 def print_fibonacci(length):
-    pass
+    if length==0:
+        print([])
+    elif length==1:
+        print([0])
+    elif length==2:
+        print([0,1])
+    else:
+         
+        for i in range(2, length):
+            fib_series.append(fib_series[-1] + fib_series[-2])
+        print(fib_series)
